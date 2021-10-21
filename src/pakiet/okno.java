d29fc4f415aaeec0d4ee6f407abfb8cad9c00107
unknown <danielll.pianka@gmail.com>
2021-10-22 01:09:59 +0200

    commit2

--- src/pakiet/okno.java
+++ src/pakiet/okno.java
@@ -1,4 +1,5 @@
 package pakiet;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
@@ -8,11 +9,14 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.RescaleOp;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -28,6 +32,7 @@ public class okno extends javax.swing.JFrame
     private void initComponents() {
 
         wybierz = new javax.swing.JFileChooser();
+        zapisz = new javax.swing.JFileChooser();
         przyciskwczytaj = new javax.swing.JButton();
         okno1 = new javax.swing.JLabel();
         okno2 = new javax.swing.JLabel();
@@ -35,6 +40,15 @@ public class okno extends javax.swing.JFrame
         jTextField2 = new javax.swing.JTextField();
         jSlider1 = new javax.swing.JSlider();
         jTextField3 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
+        jSlider2 = new javax.swing.JSlider();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        przyciskzapisz = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+
+        zapisz.setFileSelectionMode(javax.swing.JFileChooser.FILES_AND_DIRECTORIES);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Aplikacja graficzna by Daniel Pianka");
@@ -95,14 +109,70 @@ public class okno extends javax.swing.JFrame
             }
         });
 
+        jTextField4.setBackground(new java.awt.Color(255, 255, 255));
+        jTextField4.setText("               Kontrast");
+        jTextField4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField4ActionPerformed(evt);
+            }
+        });
+
+        jSlider2.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                jSlider2StateChanged(evt);
+            }
+        });
+
+        jButton1.setBackground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("BLACK&WHITE");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
+
+        jButton2.setBackground(new java.awt.Color(255, 255, 255));
+        jButton2.setText("NEGATYW");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
+
+        jButton3.setBackground(new java.awt.Color(255, 255, 255));
+        jButton3.setText("BINARYZACJA");
+        jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton3MouseClicked(evt);
+            }
+        });
+
+        przyciskzapisz.setBackground(new java.awt.Color(255, 255, 255));
+        przyciskzapisz.setText("Zapisz");
+        przyciskzapisz.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                przyciskzapiszMouseClicked(evt);
+            }
+        });
+
+        jButton5.setBackground(new java.awt.Color(204, 0, 0));
+        jButton5.setText("USUŃ EFEKTY");
+        jButton5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton5MouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(100, 100, 100)
+                .addGap(104, 104, 104)
                 .addComponent(przyciskwczytaj, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(przyciskzapisz, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(118, 118, 118))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
@@ -111,10 +181,19 @@ public class okno extends javax.swing.JFrame
                     .addGroup(layout.createSequentialGroup()
                         .addGap(103, 103, 103)
                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
-                    .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 2, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jTextField3, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
+                            .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                            .addComponent(jTextField4)))
+                    .addComponent(jSlider2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
@@ -137,9 +216,24 @@ public class okno extends javax.swing.JFrame
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                         .addComponent(okno2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(okno1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE))
-                    .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jSlider2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton5)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton2)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton3)))
                 .addGap(18, 18, 18)
-                .addComponent(przyciskwczytaj, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(przyciskzapisz, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(przyciskwczytaj, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(18, Short.MAX_VALUE))
         );
 
@@ -161,6 +255,8 @@ public class okno extends javax.swing.JFrame
         String path2 = selectedFile.getAbsolutePath();
         okno1.setIcon(ResizeImage(path));
         okno2.setIcon(ResizeImage(path2));
+        jSlider1.setValue(50);
+        jSlider2.setValue(50);
        }
        else if(result == JFileChooser.CANCEL_OPTION)
        {
@@ -183,18 +279,160 @@ public class okno extends javax.swing.JFrame
     private void jSlider1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSlider1StateChanged
         if(okno2.getIcon()==null)
         { 
-            jSlider1.setValue(50);
+        jSlider1.setValue(50);
         }
         else
         {
         JSlider slider = (JSlider) evt.getSource();
         float value = slider.getValue();
         Image image = iconToImage(okno1.getIcon());
-        image = brightnessControl(image, value);
-        Icon icon = new ImageIcon(image);
-        okno2.setIcon(icon);
+        BrightnessAndContrastControl(image, jSlider2.getValue(), value);
         }
     }//GEN-LAST:event_jSlider1StateChanged
+
+    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
+        
+    }//GEN-LAST:event_jTextField4ActionPerformed
+
+    private void jSlider2StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSlider2StateChanged
+        if(okno2.getIcon()==null)
+        { 
+        jSlider2.setValue(50);
+        }
+        else
+        {
+        JSlider slider = (JSlider) evt.getSource();
+        float value = slider.getValue();
+        Image image = iconToImage(okno1.getIcon());
+        BrightnessAndContrastControl(image, value, jSlider1.getValue());
+        }
+    }//GEN-LAST:event_jSlider2StateChanged
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        if(okno1.getIcon()==null)
+        {
+        JOptionPane.showMessageDialog(null, "Nie wybrano pliku!");
+        }
+        else
+        {
+        Image image = iconToImage(okno1.getIcon());
+        BufferedImage bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphic = bimage.createGraphics();
+            graphic.drawImage(image, 0, 0, Color.WHITE, null);
+            for (int i = 0; i < bimage.getHeight(); i++) 
+            {
+                for (int j = 0; j < bimage.getWidth(); j++) 
+                {
+                    Color c = new Color(bimage.getRGB(j, i));
+                    int red = (int) (c.getRed() * 0.299);
+                    int green = (int) (c.getGreen() * 0.587);
+                    int blue = (int) (c.getBlue() * 0.114);
+                    Color newColor = new Color(
+                    red + green + blue,
+                    red + green + blue,
+                    red + green + blue);
+                    bimage.setRGB(j, i, newColor.getRGB());
+                    Icon icon = new ImageIcon(bimage);
+                    jSlider1.setValue(50);
+                    jSlider2.setValue(50);
+                    okno2.setIcon(icon);
+                }
+            }
+        }
+    }//GEN-LAST:event_jButton1MouseClicked
+
+    private void jButton5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton5MouseClicked
+        okno2.setIcon(okno1.getIcon());
+        jSlider1.setValue(50);
+        jSlider2.setValue(50);
+    }//GEN-LAST:event_jButton5MouseClicked
+
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        if(okno1.getIcon()==null)
+        {
+        JOptionPane.showMessageDialog(null, "Nie wybrano pliku!");
+        }
+        else
+        {
+        Image image = iconToImage(okno1.getIcon());
+        BufferedImage bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphic = bimage.createGraphics();
+            graphic.drawImage(image, 0, 0, Color.WHITE, null);
+            for (int x = 0; x < bimage.getWidth(); x++) 
+            {
+            for (int y = 0; y < bimage.getHeight(); y++) 
+                {
+                int rgba = bimage.getRGB(x, y);
+                Color col = new Color(rgba, true);
+                col = new Color(255 - col.getRed(), 255 - col.getGreen(), 255 - col.getBlue());
+                bimage.setRGB(x, y, col.getRGB());
+                Icon icon = new ImageIcon(bimage);
+                jSlider1.setValue(50);
+                jSlider2.setValue(50);
+                okno2.setIcon(icon);
+                }
+            }
+        }
+    }//GEN-LAST:event_jButton2MouseClicked
+
+    private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
+        if(okno1.getIcon()==null)
+        {
+        JOptionPane.showMessageDialog(null, "Nie wybrano pliku!");
+        }
+        else
+        {
+        Image image = iconToImage(okno1.getIcon());
+        BufferedImage bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphic = bimage.createGraphics();
+        graphic.drawImage(image, 0, 0, Color.WHITE, null);
+        for(int i=0;i<okno1.getIcon().getIconWidth();i++)
+           {
+               for(int j=0;j<okno1.getIcon().getIconHeight();j++)
+               {
+                    int val = bimage.getRGB(i, j);
+                    int r = (0x00ff0000 & val) >> 16;
+                    int g = (0x0000ff00 & val) >> 8;
+                    int b = (0x000000ff & val);
+                    int m=(r+g+b);
+                    if(m>=383)
+                    {
+                        bimage.setRGB(i, j, Color.WHITE.getRGB()); 
+                    }
+                    else{  
+                        bimage.setRGB(i, j, 0);
+                    }
+                Icon icon = new ImageIcon(bimage);
+                jSlider1.setValue(50);
+                jSlider2.setValue(50);
+                okno2.setIcon(icon);
+               }
+            }  
+        }
+    }//GEN-LAST:event_jButton3MouseClicked
+
+    private void przyciskzapiszMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_przyciskzapiszMouseClicked
+        if(okno1.getIcon()==null)
+        {
+            JOptionPane.showMessageDialog(null, "Nie wybrano pliku!");
+        }
+        else
+        {
+            Icon icon = okno2.getIcon();
+            Image image = iconToImage(icon);
+            BufferedImage buffered = (BufferedImage) image;
+            try 
+            {
+            File outputfile = new File("saved.png");
+            ImageIO.write(buffered, "png", outputfile);
+            JOptionPane.showMessageDialog(null, "Plik saved.png zapisano do katalogu z projektem!");
+            }           
+            catch (IOException e) 
+            {
+            e.printStackTrace();
+            }
+        }
+    }//GEN-LAST:event_przyciskzapiszMouseClicked
 
     public static void main(String args[]) 
     {
@@ -246,30 +484,40 @@ public class okno extends javax.swing.JFrame
         }
     }
       
-      public Image brightnessControl(Image image, float brightness) 
+      public void BrightnessAndContrastControl(Image image, float brightness, float contrast) 
     {
-            BufferedImage bi = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-            Graphics bg = bi.getGraphics();
-            if (bi.getColorModel().hasAlpha()) {
-                System.out.println("Image has got an alpha channel");
-            }
-            brightness = brightness/50;
-            bg.drawImage(image, 0, 0, null);
-            bg.dispose();
-            RescaleOp rescaleOp = new RescaleOp(brightness, 0, null);
-            rescaleOp.filter(bi, bi);
-            image = bi;
-            return bi;
+        BufferedImage bi = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+        Graphics bg = bi.getGraphics();
+        if (bi.getColorModel().hasAlpha()) 
+        {
+        System.out.println("Image has got an alpha channel");
+        }
+        brightness = (brightness/50);
+        bg.drawImage(image, 0, 0, null);
+        bg.dispose();
+        RescaleOp rescaleOp = new RescaleOp(brightness, contrast, null);
+        rescaleOp.filter(bi, bi);
+        image = bi;
+        Icon icon = new ImageIcon(bi);
+        okno2.setIcon(icon);
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton5;
     private javax.swing.JSlider jSlider1;
+    private javax.swing.JSlider jSlider2;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
     private javax.swing.JLabel okno1;
     private javax.swing.JLabel okno2;
     private javax.swing.JButton przyciskwczytaj;
+    private javax.swing.JButton przyciskzapisz;
     private javax.swing.JFileChooser wybierz;
+    private javax.swing.JFileChooser zapisz;
     // End of variables declaration//GEN-END:variables
 }
